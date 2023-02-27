f6c686e9dccf54793234d5c5acd9561d2917a4c9
YulianaMu <neyumi24@gmail.com>
2023-02-26 23:35:42 -0500

    Trabajo - Corte #1

---
+++ src/models/index.ts
@@ -0,0 +1,2 @@
+export * from './factura.model';
+export * from './productos.model';
---
+++ src/models/productos.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Productos extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  detalle: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Productos>) {
+    super(data);
+  }
+}
+
+export interface ProductosRelations {
+  // describe navigational properties here
+}
+
+export type ProductosWithRelations = Productos & ProductosRelations;
